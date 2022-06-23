fb3d931276f332e158f1db3562988d331eb3f24d
Kevin McIntosh <kmac303@gmail.com>
2022-06-23 10:18:29 -0600

    commit tree 2d0b78da0181200a901e236dc67a3d30eb2c29e4

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+let employee = {
+    name: "Kevin",
+    streetAddress: "123 Park Ave",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let newEmployee = { ...employee, ...{ [key]: value } }
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObj = { ...employee }
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
